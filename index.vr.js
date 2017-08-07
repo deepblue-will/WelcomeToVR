c1f461e5f56b933981467d2afb45e19658ca9638
deepblue_will <deepblue.will@gmail.com>
2017-08-07 23:50:40 +0900

    :+1: Apply transform

--- index.vr.js
+++ index.vr.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {
   AppRegistry,
   asset,
@@ -7,43 +7,26 @@ import {
   View,
 } from 'react-vr';
 
-class Greeting extends Component {
-  render() {
-    return (
-      <Text>Hello {this.props.name}!</Text>
-    );
-  }
-}
-
 export default class WelcomeToVR extends React.Component {
   render() {
     return (
-    <View>
-      <Pano source={asset('chess-world.jpg')} />
-      <View style={{
-        flex: 1,
-        flexDirection: 'column',
-        width: 2,
-        alignItems: 'stretch',
-        transform: [{translate: [-1, 1, -5]}],
-      }}>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red'}}>
-          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Red</Text>
-        </View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'orange'}}>
-          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Orange</Text>
-        </View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'yellow'}}>
-          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Yellow</Text>
-        </View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'green'}}>
-          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Green</Text>
-        </View>
-        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'blue'}}>
-          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Blue</Text>
-        </View>
+      <View>
+        <Pano source={asset('chess-world.jpg')}/>
+        <Text
+          style={{
+            backgroundColor: '#777879',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [1, 0, -10]}, {rotateX : -30}, {rotateZ: 90}, {rotateY: 30}, {scale: 0.6}],
+          }}>
+          hello
+        </Text>
       </View>
-    </View>
     );
   }
 };
