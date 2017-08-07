b9b1560875faa26034b66a1c81c235a3e96770c2
deepblue_will <deepblue.will@gmail.com>
2017-08-08 01:02:07 +0900

    :+1: escape

--- index.vr.js
+++ index.vr.js
@@ -9,6 +9,22 @@ import {
 } from 'react-vr';
 
 export default class WelcomeToVR extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      translate: [0, 0, -10]
+    }
+  }
+
+  escape() {
+    this.setState({translate: [this.randomNumber(-10, 10), this.randomNumber(-10, 10), -10]})
+  }
+
+  randomNumber(min, max) {
+    return Math.floor( Math.random() * (max + 1 - min) ) + min;
+  }
+
   render() {
     return (
       <View>
@@ -20,8 +36,10 @@ export default class WelcomeToVR extends React.Component {
               width: 3, 
               height: 4,
               layoutOrigin: [0.5, 0.5],
-              transform: [{translate: [0, 0, -10]}]
-            }} />
+              transform: [{translate: this.state.translate}]
+            }} 
+            onEnter={this.escape.bind(this)}
+            />
         </View>
       </View>
     );
