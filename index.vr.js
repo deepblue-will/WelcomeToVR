a44d1ed52095c8543e2323d8f2ebd367035c9dfc
deepblue_will <deepblue.will@gmail.com>
2017-08-07 23:01:43 +0900

    :+1: Apply some styles

--- index.vr.js
+++ index.vr.js
@@ -18,14 +18,32 @@ class Greeting extends Component {
 export default class WelcomeToVR extends React.Component {
   render() {
     return (
-      <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <View style={{transform: [{translate: [0, 0, -3]}]}}>
-          <Greeting name='Rexxar' />
-          <Greeting name='Jaina' />
-          <Greeting name='Valeera' />
+    <View>
+      <Pano source={asset('chess-world.jpg')} />
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        width: 2,
+        alignItems: 'stretch',
+        transform: [{translate: [-1, 1, -5]}],
+      }}>
+        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'red'}}>
+          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Red</Text>
+        </View>
+        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'orange'}}>
+          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Orange</Text>
+        </View>
+        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'yellow'}}>
+          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Yellow</Text>
+        </View>
+        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'green'}}>
+          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Green</Text>
+        </View>
+        <View style={{ margin: 0.1, height: 0.3, backgroundColor: 'blue'}}>
+          <Text style={{fontSize: 0.2, textAlign: 'center'}}>Blue</Text>
         </View>
       </View>
+    </View>
     );
   }
 };
