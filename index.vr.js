d7b8157a002b9ea4d4e36f6ccd1ce8fbeb50c095
deepblue_will <deepblue.will@gmail.com>
2017-08-07 22:56:01 +0900

    :+1: Add props and another component

--- index.vr.js
+++ index.vr.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
   AppRegistry,
   asset,
@@ -7,25 +7,24 @@ import {
   View,
 } from 'react-vr';
 
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
 export default class WelcomeToVR extends React.Component {
   render() {
     return (
       <View>
         <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+        <View style={{transform: [{translate: [0, 0, -3]}]}}>
+          <Greeting name='Rexxar' />
+          <Greeting name='Jaina' />
+          <Greeting name='Valeera' />
+        </View>
       </View>
     );
   }
