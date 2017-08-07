40d75adea306fc53cf060227f64fd118f752c5bb
deepblue_will <deepblue.will@gmail.com>
2017-08-08 00:55:07 +0900

    :+1: Add image

--- index.vr.js
+++ index.vr.js
@@ -5,36 +5,24 @@ import {
   Pano,
   Text,
   View,
+  Image,
 } from 'react-vr';
 
 export default class WelcomeToVR extends React.Component {
-  constructor() {
-    super();
-    this.state = {textColor: 'white'};
-  }
-
   render() {
     return (
       <View>
         <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            color: this.state.textColor,
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -6]}],
-          }}
-          onEnter={() => this.setState({textColor: 'red'})}
-          onExit={() => this.setState({textColor: 'white'})}
-          >
-          hello
-        </Text>
+        <View style={{display: 'flex', flexDirection:'column'}}>
+          <Image
+            source={asset('urapico.jpg')}
+            style={{
+              width: 3, 
+              height: 4,
+              layoutOrigin: [0.5, 0.5],
+              transform: [{translate: [0, 0, -10]}]
+            }} />
+        </View>
       </View>
     );
   }
